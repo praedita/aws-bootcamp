6905a7f0d2bbc018e13090962a75e68794f94be2
praedita <joanna.solecka@gmail.com>
2023-12-13 17:01:04 +0100

    add variable and provider files

--- main.tf
+++ main.tf
@@ -69,7 +69,7 @@ resource "aws_subnet" "capstone-dev-db-private-1b-1" {
   availability_zone       = data.aws_availability_zones.available.names[1]
   map_public_ip_on_launch = false
   tags = {
-    Name = "capstone-dev-db-private-1b-1"
+    Name = "capstonePrivateSubnet1b"
   }
   provisioner "local-exec" {
     command = "echo private subnet 1b = ${self.id} >> metadata"
@@ -83,4 +83,66 @@ resource "aws_internet_gateway" "capstone-dev-igw" {
   tags = {
     Name = "capstoneIGW"
   }
+}
+
+# Allocate an Elastic IP for the NAT Gateway
+resource "aws_eip" "capstone-dev-eip" {
+  vpc = true
+}
+
+# Create a NAT Gateway for the private subnet(s) to access the internet
+resource "aws_nat_gateway" "capstone-dev-nat-gw" {
+  allocation_id = aws_eip.capstone-dev-eip.id
+  subnet_id     = aws_subnet.capstone-dev-web-public-1a-1.id # Reference public subnet ID
+
+  tags = {
+    Name = "capstoneNatGW"
+  }
+}
+
+resource "aws_route_table" "capstone-dev-public-rt" {
+  vpc_id = aws_vpc.capstone-dev-vpc.id
+
+  route {
+    cidr_block = var.cidr_blocks
+    gateway_id = aws_internet_gateway.capstone-dev-igw.id
+  }
+  tags = {
+    Name = "capstonePublicRouteTable"
+  }
+}
+
+resource "aws_route_table" "capstone-dev-private-rt" {
+  vpc_id = aws_vpc.capstone-dev-vpc.id
+
+  route {
+    cidr_block = var.cidr_blocks
+    gateway_id = aws_nat_gateway.capstone-dev-nat-gw.id
+  }
+  tags = {
+    Name = "capstonePrivateRouteTable"
+  }
+}
+resource "aws_route_table_association" "capstone-public-1a-rt-association" {
+  route_table_id = aws_route_table.capstone-dev-public-rt.id
+  subnet_id      = aws_subnet.capstone-dev-web-public-1a-1.id
+  depends_on     = [aws_route_table.capstone-dev-public-rt, aws_subnet.capstone-dev-web-public-1a-1]
+}
+
+resource "aws_route_table_association" "capstone-private-1a-rt-association" {
+  route_table_id = aws_route_table.capstone-dev-private-rt.id
+  subnet_id      = aws_subnet.capstone-dev-db-private-1a-1.id
+  depends_on     = [aws_route_table.capstone-dev-private-rt, aws_subnet.capstone-dev-db-private-1a-1]
+}
+
+resource "aws_route_table_association" "capstone-public-1b-rt-association" {
+  route_table_id = aws_route_table.capstone-dev-public-rt.id
+  subnet_id      = aws_subnet.capstone-dev-web-public-1b-1.id
+  depends_on     = [aws_route_table.capstone-dev-public-rt, aws_subnet.capstone-dev-web-public-1b-1]
+}
+
+resource "aws_route_table_association" "capstone-private-1b-rt-association" {
+  route_table_id = aws_route_table.capstone-dev-private-rt.id
+  subnet_id      = aws_subnet.capstone-dev-db-private-1b-1.id
+  depends_on     = [aws_route_table.capstone-dev-private-rt, aws_subnet.capstone-dev-db-private-1b-1]
 }
---
+++ variables.tf
@@ -0,0 +1,28 @@
+variable "aws_region" {
+  default     = "us-west-2"
+  description = "AWS region"
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+# Network Mask - 255.255.255.0 Addresses Available - 256
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+variable "publ_cidr_1" {
+  default = "10.0.1.0/24" # 256 IPs
+}
+variable "priv_cidr_1" {
+  default = "10.0.2.0/24" # 256 IPs
+}
+variable "publ_cidr_2" {
+  default = "10.0.3.0/24" # 256 IPs
+}
+variable "priv_cidr_2" {
+  default = "10.0.4.0/24" # 256 IPs
+}
+variable "cidr_blocks" {
+  default = "0.0.0.0/0"
+}
