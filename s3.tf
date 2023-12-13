9f8fa187670f01a2c10aec0f4187bb3283596986
praedita <joanna.solecka@gmail.com>
2023-12-13 17:22:02 +0100

    create a bucket for load balancer logs

---
+++ s3.tf
@@ -0,0 +1,16 @@
+# Create an S3 bucket
+resource "aws_s3_bucket" "capstone-dev-alblogs-bucket" {
+  bucket = "capstone-dev-alblogs-bucket"
+
+  tags = {
+    Name        = "capstoneAlbLogsBucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "capstone-dev-alblogs-bucket-versioning" {
+  bucket = aws_s3_bucket.capstone-dev-alblogs-bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
