9d19ebcad932966e5fea48f80873dc2315b53c01
praedita <joanna.solecka@gmail.com>
2023-12-15 17:35:24 +0100

    comment out obsolete code

--- s3.tf
+++ s3.tf
@@ -1,16 +1,17 @@
-# Create an S3 bucket
-resource "aws_s3_bucket" "capstone-dev-alblogs-bucket" {
-  bucket = "capstone-dev-alblogs-bucket"
+# # Create an S3 bucket
+# resource "aws_s3_bucket" "capstone-dev-alblogs-bucket" {
+#   bucket = "capstone-dev-alblogs-bucket"
 
-  tags = {
-    Name        = "capstoneAlbLogsBucket"
-    Environment = "Dev"
-  }
-}
+#   tags = {
+#     Name        = "capstoneAlbLogsBucket"
+#     Environment = "Dev"
+#   }
+# }
 
-resource "aws_s3_bucket_versioning" "capstone-dev-alblogs-bucket-versioning" {
-  bucket = aws_s3_bucket.capstone-dev-alblogs-bucket.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
+# # Enable versioning on the S3 bucket
+# resource "aws_s3_bucket_versioning" "capstone-dev-alblogs-bucket-versioning" {
+#   bucket = aws_s3_bucket.capstone-dev-alblogs-bucket.id
+#   versioning_configuration {
+#     status = "Enabled"
+#   }
+# }
